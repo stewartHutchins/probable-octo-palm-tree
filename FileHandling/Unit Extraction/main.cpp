637f8b7ecee5797fb57f9c400c97f646c5c7fecd
Stewart <stewartmhutchins@gmail.com>
2018-01-28 17:04:09 +0000

    Unit Extraction

---
+++ FileHandling/Unit Extraction/main.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <stdexcept>
+
+using namespace std;
+
+//function declaration
+bool compareVariable(string* line, const string varName);
+bool isProductQuan(string* line);
+bool isWeightUnit(string str);
+bool isEnd(string* str);
+string cleanStr(string line);
+bool isUnitType(string str);
+int stringToInt(string str);
+
+
+static const string inFileName= "./../food-enforcement_REPEATS_REMOVED.json";
+static const string outFileName= "./../food-enforcement_UNITS_Inc_RR.json";
+static const string productQuanStr= "      \"product_quantity\": \"";
+
+static const string weightTypes[] = {"oz", "lb", "pound", "gallon", "litre", "total", "pint"};
+static const string unitTypes[] = {"units", "bottles", "cakes", "cookies", "pies", "jars", "tins", "cans", "cartons","ct", "sandwiches"};
+
+int main()
+{
+    ifstream foodFile(inFileName);
+    ofstream outFile(outFileName);
+    if(!foodFile.is_open())
+    {
+        throw runtime_error("Error opening foodFile");
+    }
+    else if(!outFile.is_open())
+    {
+        throw runtime_error("Error opening outFile");
+    }
+
+    string line;
+    int units;
+    int cases;
+    int boxes;
+    int noUnitType=0;
+    while(getline(foodFile, line))
+    {
+        if(isProductQuan(&line))
+        {
+            string cleanedLine = cleanStr(line);
+            stringstream ss(cleanedLine);
+            string quantity;
+            string unitType;
+            units=0;
+            cases=0;
+            boxes=0;
+            while(ss >>unitType)
+            {
+                /*
+                if(isWeightUnit(unitType))
+                {
+                    ss >> unitType;
+                }
+                */
+                if(isUnitType(unitType))
+                {
+                    units +=stringToInt(quantity);
+                }
+                else if(unitType.find("case")!=string::npos ||unitType.find("cs")!=string::npos)
+                {
+                    cases +=stringToInt(quantity);
+                }
+                else if(unitType.find("box")!=string::npos)
+                {
+                    boxes +=stringToInt(quantity);
+                }
+                quantity = unitType;
+            }
+        }
+        else if(isEnd(&line))
+        {
+            if(units==0 && boxes==0 && cases ==0){
+                ++noUnitType;
+            }
+
+            outFile << "," <<endl;
+            outFile << "      \"units\": \"";
+            if(units!=0)
+            {
+                outFile << units;
+            }
+            outFile<< "\"," << endl;
+            outFile << "      \"cases\": \"";
+            if(cases!=0)
+            {
+                outFile << cases;
+            }
+            outFile <<"\"," << endl;
+            outFile << "      \"boxes\": \"";
+            if(boxes !=0)
+            {
+                outFile << boxes;
+            }
+            outFile << "\"" << endl;
+        }
+        outFile << line << endl;
+    }
+
+    foodFile.close();
+    outFile.close();
+
+    cout <<"There were " << noUnitType << " instances where no unit type could be derived" << endl;
+    cout << "END" << endl;
+    return 0;
+}
+
+
+bool compareVariable(string* line, const string varName)
+{
+    return (line->substr(0, varName.length())==varName);
+}
+
+bool isProductQuan(string* line)
+{
+    return compareVariable(line, productQuanStr);
+}
+
+bool isEnd(string* line)
+{
+    return (*line=="    },");
+}
+
+
+int stringToInt(string str)
+{
+    cout << str <<endl;
+    stringstream ss(str);
+    int i;
+    ss >> i;
+    if(ss.fail())
+    {
+        return 0;
+    }
+    else
+    {
+        return i;
+    }
+}
+
+bool isWeightUnit(string str)
+{
+    for(unsigned i=0; i<sizeof(weightTypes)/sizeof(weightTypes[0]); ++i)
+    {
+        if(str.find(weightTypes[i]) !=string::npos)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isUnitType(string str)
+{
+    for(unsigned i=0; i<sizeof(unitTypes)/sizeof(unitTypes[0]); ++i)
+    {
+        if(str ==unitTypes[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+string cleanStr(string str)
+{
+    for(string::iterator i = str.begin(); i != str.end(); i++)
+    {
+        if(!isalnum(str.at(i - str.begin())) && str.at(i - str.begin()) != ' ')
+        {
+            str.erase(i);
+            --i;
+        }
+    }
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return str;
+}
+
+
