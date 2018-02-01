6ce689597a2c6c1019e24aaeb323be25159194d5
Stewart <stewartmhutchins@gmail.com>
2018-02-01 17:12:30 +0000

    Sample created to check effectiveness of method

--- FileHandling/Create Sample/main.cpp
+++ FileHandling/Create Sample/main.cpp
@@ -7,14 +7,10 @@
 
 using namespace std;
 
-static const string dataFileName= "./../food-enforcement.json";
-static const string outFileName= "./200sample.txt";
-
-static const string recallReasonStr="      \"reason_for_recall\": \"";
-static const string productDescStr="      \"product_description\": \"";
+static const string dataFileName= "./../food-enforcement_FOODTYPE_AND_REASON_RR_UNITS.json";
+static const string outFileName= "./../200sampleFOODTYPE_AND_REASON_RR_UNITS.json";
 
 static double numberOfSamplesWanted = 200.0;
-static double proportion = numberOfSamplesWanted/14566.0;
 
 void toLowerCase(string* str );
 
@@ -23,36 +19,67 @@ int main()
     ifstream dataFile(dataFileName);
     ofstream outFile(outFileName);
 
-    if(!dataFile.is_open() ){
+    if(!dataFile.is_open() )
+    {
         throw runtime_error("Error opening dataFile");
-    }else if(!outFile.is_open()){
+    }
+    else if(!outFile.is_open())
+    {
         throw runtime_error("Error opening outFile");
     }
 
-    double random;
     string line;
-    while(getline(dataFile, line)){
-        random = ((double)rand() / (RAND_MAX));
-        if(random <= proportion){
-            if(line.substr(0,recallReasonStr.length())==recallReasonStr){
-                toLowerCase(&line);
-                outFile << "\n" << line <<"\n" <<"RECALL_REASON_SHORT: " <<"\n";
-                cout <<line <<endl;
-            }else if(line.substr(0,productDescStr.length())==productDescStr){
-                toLowerCase(&line);
-                outFile <<"\n"<< line <<"\n" <<"PRODUCT_SHORT: " <<"\n";
-                cout <<line <<endl;
-            }
+    int eventCount=0;
+    while(getline(dataFile, line))
+    {
+        if(line == "    {")
+        {
+            ++eventCount;
         }
     }
+    dataFile.close();
+    dataFile.open(dataFileName);
+    if(!outFile.is_open())
+    {
+        throw runtime_error("Error opening outFile, 2nd time");
+    }
 
+    outFile << "{" <<endl;
+    double random;
+    double probability;
+    double checkCount =0;
+    while(getline(dataFile, line))
+    {
+        probability = numberOfSamplesWanted / (double)eventCount;
+        random = ((double)rand() / (RAND_MAX));
+        if(line == "    {")
+        {
+            if(random <= probability)
+            {
+                outFile <<line <<endl;
+                while(getline(dataFile, line))
+                {
+                    outFile << line <<endl;
+                    if(line=="    },")
+                    {
+                        ++checkCount;
+                        --numberOfSamplesWanted;
+                        break;
+                    }
+                }
+            }
+            --eventCount;
+        }
+    }
+    outFile << "  ]\n}";
     dataFile.close();
     outFile.close();
-
+    cout << "Number of Samples: " << checkCount << endl;
     cout << "END" << endl;
     return 0;
 }
 
-void toLowerCase(string* str ){
+void toLowerCase(string* str )
+{
     transform(str->begin(), str->end(), str->begin(), ::tolower);
 }
